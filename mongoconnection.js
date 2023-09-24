260c3e2ae132dde09c112a701927b8a963c25200
Sachin Sharma <sachin.sharma3@incedoinc.com>
2023-09-24 16:04:25 +0530

    mongodb atlas integrated

--- mongoconnection.js
+++ mongoconnection.js
@@ -1,5 +1,12 @@
-var mongoose = require("mongoose");
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
 
-mongoose.connect('mongodb://localhost:27017/STAR-APP').then(() => {
+var mongoose = require("mongoose");
+var dbURL = process.env.DB_URL
+var dbLocal = 'mongodb://localhost:27017/STAR-APP'
+
+
+mongoose.connect(dbURL).then(() => {
     console.log("Connected to database through mongoose.")
 });
